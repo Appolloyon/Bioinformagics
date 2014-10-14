47e837fdcb37647e7389aca5163182f4909ca62d
Appolloyon <cklinger@ualberta.ca>
2014-10-14 03:21:21 -0600

    Merged multiple seq classes into single class

--- codon_pref.py
+++ codon_pref.py
@@ -1,38 +1,78 @@
 #!/usr/bin/env python
 
-class Seq():
-	'Class string documentation'
-	def __init__(self):
-		pass
+import re
+import sys
 
-	def index(self):
-		pass
-	
-	def lookup(self):
-		pass
+class Seq(object):
+    """Class string documentation"""
 
-class Nuc_Seq(Seq):
-	'Class string documentation'
-	def __init__(self):
-		pass
+    aa_index = 0
+    nuc_index = 0
 
-	pass
+    def __init__(self, seq1, seq2):
+        self.nuc_seq = seq1
+        self.aa_seq = seq2
 
-class Prot_Seq(Seq):
-	'Class string documentation'
-	def __init__(self):
-		pass
+    def index_aa(self):
+        return aa_index
+        aa_index += 1
+
+    def index_nuc(self):
+        return nuc_index
+        nuc_index += 3
+
+	def lookup_aa(self, i=aa_index, seq=aa_seq):
+        for i, n in enumerate(seq):
+            return n
+
+    def lookup_nuc(self, i=nuc_index, seq=nuc_seq):
+        for i, n in enumerate(seq):
+            return [n:n+3]
 
 	def check_codon(self):
-		pass
+        for codon in lookup_nuc(index_nuc):
+            return codon
 
 	def update_codons(self):
 		pass
 
+nuc_file = sys.argv[1]
+prot_file = sys.argv[2]
+
+with open(nuc_file, 'U') as f1, open(prot_file, 'U') as f2:
+    nuc_dict = {}
+    prot_dict = {}
+    for curline in f1:
+        if curline.startswith(">"):
+            curline = curline.strip(">").strip("\n")
+            ID = curline
+            nuc_dict[ID] = ''
+        else:
+            curline = curline.strip("\n")
+            nuc_dict[ID] += curline
+
+     for curline in f2:
+        if curline.startswith(">"):
+            curline = curline.strip(">").strip("\n")
+            ID = curline
+            prot_dict[ID] = ''
+        else:
+            curline = curline.strip("\n")
+            prot_dict[ID] += curline
+
+    for a,b in enumerate(nuc_dict.keys(), prot_dict.keys()):
+        counter = 0
+        seq_list = []
+        if re.search(a, b):
+            `counter` = Seq(nuc_dict.get(a), prot_dict.get(b)
+            seq_list.append(`counter`)
+            counter += 1
+
+
 """
 read in aligned nucleotide and protein seq files
 
-parse each as per other programs 
+parse each as per other programs
 
 write a loop to initialize a new object for each sequence with the first full
 word of the description line as the var name and the sequence as one of the
@@ -51,6 +91,6 @@ no matter what though, advance the amino acid sequence index forward one
 
 continue on in this manner until the end of each sequence is reached
 
-write output to a file which has the sequence ID and the associated list of 
+write output to a file which has the sequence ID and the associated list of
 	codons and the number of times they are used
-""" 
+"""
